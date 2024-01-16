bd938f8eda5015bea7e51a556909ac02929cb386
HANZALA SALEEM <96341952+HANZALASALEEM@users.noreply.github.com>
2024-01-16 16:45:21 +0500

    16/1/2024

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginScreen from "./screens/loginScreen/LoginScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<>
+			<div className="App">
+				{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +18,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+				<LoginScreen />
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ web/src/screens/loginScreen/LoginScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoginScreen.css";
+
+export default function LoginScreen() {
+	return (
+		<>
+			<div className="body">
+				<div className="leftContainer"></div>
+				<div className="rightContainer"></div>
+			</div>
+		</>
+	);
+}
