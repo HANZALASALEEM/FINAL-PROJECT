5e99e16d8299314e837338c2bde10feda61c918d
HANZALA SALEEM <96341952+HANZALASALEEM@users.noreply.github.com>
2024-01-17 22:46:01 +0500

    17/1/2024

---
+++ web/src/firebase/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAEgweJHDKF_B_SHeZe4v7BgrEYm2CBrv4",
+	authDomain: "suffah-model-school-fed72.firebaseapp.com",
+	projectId: "suffah-model-school-fed72",
+	storageBucket: "suffah-model-school-fed72.appspot.com",
+	messagingSenderId: "46075682266",
+	appId: "1:46075682266:web:4a5d31c11f9b089b38b4e3",
+	measurementId: "G-JN75VETP1E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
--- web/src/screens/loginScreen/LoginScreen.jsx
+++ web/src/screens/loginScreen/LoginScreen.jsx
@@ -1,12 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import { db } from "../../firebase/firebase.config";
+import { getDocs, collection, query } from "firebase/firestore";
 
 export default function LoginScreen() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const login = async (email, password) => {
+		// Step 1: Query the Firestore collection "Administration" to get all documents
+		const adminCollectionRef = collection(db, "Administration");
+		const adminQuery = query(adminCollectionRef);
+
+		try {
+			const querySnapshot = await getDocs(adminQuery);
+
+			// Step 2: Iterate through the documents
+			querySnapshot.forEach((doc) => {
+				const adminData = doc.data();
+
+				// Step 3: Check if any document has the provided email and password
+				if (
+					adminData.email === "hanzala@suffah.edu.pk" ||
+					adminData.password === password
+					// >>>>>>>>>>>>>>>>>>>>>ERROR hy yahan<<<<<<<<<<<<<<<<<<<<<
+				) {
+					// If a match is found, allow the user to proceed to the next screen
+					console.log("match found"); // Function to handle user authentication
+				} else {
+					// If no match is found
+					console.log("Invalid email or password");
+				}
+			});
+		} catch (error) {
+			console.error("Error getting documents: ", error);
+		}
+	};
 	return (
 		<>
 			<div className="body">
-				<div className="leftContainer"></div>
-				<div className="rightContainer"></div>
+				<div className="leftContainer">
+					<img
+						className="suffahLogo"
+						src={require("../../assets/images/suffah-logo.png")}
+						alt="Suffah Logo"
+					/>
+				</div>
+				<div className="rightContainer">
+					<h1 className="wellcomeHeading">Wellcome!</h1>
+					<h1 className="nameHeading">Suffah Model School</h1>
+					<div className="formContainer">
+						{/* Email Input  */}
+						<div className="lableAndInputContainer">
+							<p className="lable">Email: </p>
+							<input
+								className="textInput"
+								placeholder="Aministrative Email"
+								value={email}
+								onChange={(text) => {
+									setEmail(text.target.value);
+								}}
+							/>
+						</div>
+						{/* Password Input */}
+						<div className="lableAndInputContainer mt-2">
+							<p className="lable">Password: </p>
+							<input
+								className="textInput"
+								placeholder="Password"
+								type="password"
+								value={password}
+								onChange={(text) => {
+									setPassword(text.target.value);
+								}}
+							/>
+						</div>
+						{/* Login Button */}
+						<button type="button" className="loginButton" onClick={login}>
+							Login
+						</button>
+					</div>
+				</div>
 			</div>
 		</>
 	);
