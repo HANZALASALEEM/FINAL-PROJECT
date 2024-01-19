45c937000c613cb1d14bb1d4447a34c96aa034aa
HANZALA SALEEM <96341952+HANZALASALEEM@users.noreply.github.com>
2024-01-19 12:17:28 +0500

    19/1/2024

--- web/src/component/sideBar/SideBar.jsx
+++ web/src/component/sideBar/SideBar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
 import { Link } from "react-router-dom";
+import { Button, Drawer } from "antd";
 function SideBar() {
+	const [open, setOpen] = useState(false);
+	const showDrawer = () => {
+		setOpen(true);
+	};
+	const onClose = () => {
+		setOpen(false);
+	};
 	return (
 		<div className="sideBarContainer">
-			<div className="logoAndTitle">
+			{/* <div className="logoAndTitle">
 				<img
 					src={require("../../assets/images/suffah-logo.png")}
 					className="logo"
@@ -13,8 +21,23 @@ function SideBar() {
 			</div>
 			<div className="navbarContainer">
 				<div className="navLink">
-					<Link to="/StudentList">Student</Link>
+					<Link to="./StudentList">Student</Link>
 				</div>
+			</div> */}
+			<div>
+				<Button type="primary" onClick={showDrawer}>
+					Open
+				</Button>
+				<Drawer
+					title="Basic Drawer"
+					onClose={onClose}
+					open={open}
+					placement="left"
+				>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Drawer>
 			</div>
 		</div>
 	);
--- web/src/screens/studentScreen/studentList/StudentListScreen.jsx
+++ web/src/screens/studentScreen/studentList/StudentListScreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./StudentListScreen.css";
 export default function StudentListScreen() {
 	return (
-		<div>
+		<div className="body">
 			<p>this is student list</p>
 		</div>
 	);
